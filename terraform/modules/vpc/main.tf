46abeb8646099d26462ab77ce09c5a590fd49f81
Kuniaki Shimizu <kun432.8d1w@gmail.com>
2021-08-10 01:49:18 +0900

    NAT-GWを追加

--- terraform/modules/vpc/main.tf
+++ terraform/modules/vpc/main.tf
@@ -84,24 +84,81 @@ resource "aws_route" "route_igw" {
   route_table_id         = aws_route_table.rtb_public.id
   destination_cidr_block = "0.0.0.0/0"
   gateway_id             = aws_internet_gateway.igw.id
-  depends_on             = [aws_route_table.rtb_public]
+  depends_on             = [aws_internet_gateway.igw]
 }
 
-resource "aws_route_table" "rtb_private" {
+resource "aws_route_table" "rtb_private_c" {
   vpc_id = aws_vpc.vpc.id
   tags = {
-    Name = "${var.prj_name}-rtb-private"
+    Name = "${var.prj_name}-rtb-private-c"
+  }
+}
+
+resource "aws_route_table" "rtb_private_d" {
+  vpc_id = aws_vpc.vpc.id
+  tags = {
+    Name = "${var.prj_name}-rtb-private-d"
   }
 }
 
 resource "aws_route_table_association" "rtb_assoc_private_c" {
-  route_table_id = aws_route_table.rtb_private.id
+  route_table_id = aws_route_table.rtb_private_c.id
   subnet_id      = aws_subnet.subnet_private_c.id
 }
 
 resource "aws_route_table_association" "rtb_assoc_private_d" {
-  route_table_id = aws_route_table.rtb_private.id
+  route_table_id = aws_route_table.rtb_private_d.id
   subnet_id      = aws_subnet.subnet_private_d.id
+}
+
+resource "aws_eip" "eip_natgw_c" {
+  vpc = true
+
+  tags = {
+    Name = "${var.prj_name}-eip-natgw-c"
+  }
+}
+resource "aws_eip" "eip_natgw_d" {
+  vpc = true
+
+  tags = {
+    Name = "${var.prj_name}-eip-natgw-d"
+  }
+}
+
+resource "aws_nat_gateway" "natgw_c" {
+  allocation_id = aws_eip.eip_natgw_c.id
+  subnet_id     = aws_subnet.subnet_public_c.id
+
+  tags = {
+    Name = "${var.prj_name}-natgw-c"
+  }
+
+  depends_on = [aws_internet_gateway.igw]
+}
+
+resource "aws_nat_gateway" "natgw_d" {
+  allocation_id = aws_eip.eip_natgw_d.id
+  subnet_id     = aws_subnet.subnet_public_d.id
+
+  tags = {
+    Name = "${var.prj_name}-natgw-d"
+  }
+
+  depends_on = [aws_internet_gateway.igw]
+}
+resource "aws_route" "route_natgw_c" {
+  route_table_id         = aws_route_table.rtb_private_c.id
+  destination_cidr_block = "0.0.0.0/0"
+  gateway_id             = aws_nat_gateway.natgw_c.id
+  depends_on             = [aws_nat_gateway.natgw_c]
+}
+
+resource "aws_route" "route_natgw_d" {
+  route_table_id         = aws_route_table.rtb_private_d.id
+  destination_cidr_block = "0.0.0.0/0"
+  gateway_id             = aws_nat_gateway.natgw_d.id
+  depends_on             = [aws_nat_gateway.natgw_d]
 }
 
 output vpc_id {
@@ -119,6 +176,6 @@ output private_subnet_ids {
 output public_route_table_id {
   value = aws_route_table.rtb_public.id
 }
-output private_route_table_id {
-  value = aws_route_table.rtb_private.id
+output private_route_table_ids {
+  value = [aws_route_table.rtb_private_c.id, aws_route_table.rtb_private_d.id]
 }
